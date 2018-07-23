2a0b2ffa9cce47dfe02146f7b53e65f67ee4a3a1
lixuanqi <lixuanqi1995@gmail.com>
2018-07-23 15:53:22 +0800

    add 'LocationRepository'

---
+++ location-service/src/main/java/app/domain/LocationRepository.java
@@ -0,0 +1,13 @@
+package app.domain;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+// JpaRepository<Entity, id>
+public interface LocationRepository extends JpaRepository<Location, Long> {
+    Page<Location> findByRunnerMovementType(@Param("movementType") Location.RunnerMovementType movementType, Pageable pageable);
+}
+
+// pagination, how to implement by sql.
