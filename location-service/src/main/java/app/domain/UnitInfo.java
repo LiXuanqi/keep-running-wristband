f57f3feadf6ba8b892c7e2f8a785864031f9af00
lixuanqi <lixuanqi1995@gmail.com>
2018-07-23 12:34:39 +0800

    add UnitInfo domain.

---
+++ location-service/src/main/java/app/domain/UnitInfo.java
@@ -0,0 +1,24 @@
+package app.domain;
+
+public class UnitInfo {
+    private final String runningId;
+    private String bandMake;
+    private String customerName;
+    private String unitNumber;
+
+    public UnitInfo() {
+        this.runningId = "";
+    }
+
+    public UnitInfo(String runningId) {
+        this.runningId = runningId;
+    }
+
+    public UnitInfo(String runningId, String bandMake, String customerName, String unitNumber) {
+        this.runningId = runningId;
+        this.bandMake = bandMake;
+        this.customerName = customerName;
+        this.unitNumber = unitNumber;
+    }
+
+}
