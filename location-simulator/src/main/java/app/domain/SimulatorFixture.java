ac93d63f876e28eb9f28f17d41611ca1afba274a
lixuanqi <lixuanqi1995@gmail.com>
2018-07-27 00:39:25 +0800

    add domains

---
+++ location-simulator/src/main/java/app/domain/SimulatorFixture.java
@@ -0,0 +1,32 @@
+package app.domain;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@JsonPropertyOrder({"numberOfGpsSimulatorRequests", "gpsSimulatorRequests"})
+public class SimulatorFixture {
+    private List<GpsSimulatorRequest> gpsSimulatorRequests = new ArrayList<GpsSimulatorRequest>(0);
+
+    public int getNumberofGpsSimulatorRequests() {
+        return gpsSimulatorRequests.size();
+    }
+
+    public void setGpsSimulatorRequests(List<GpsSimulatorRequest> gpsSimulatorRequests) {
+        Assert.notEmpty(gpsSimulatorRequests, "gpsSimulatorRequests must not be empty.");
+        this.gpsSimulatorRequests = gpsSimulatorRequests;
+    }
+
+    public boolean usesKmlIntegration() {
+        return gpsSimulatorRequests.stream().anyMatch(request -> request.isExportPositionsToKml());
+    }
+}
