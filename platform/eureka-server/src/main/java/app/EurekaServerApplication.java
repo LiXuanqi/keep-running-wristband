9ba9fdeab474fd6bc7fe8b0db0687907e529fc89
lixuanqi <lixuanqi1995@gmail.com>
2018-07-27 03:37:16 +0800

    delete useless properties.

--- platform/eureka-server/src/main/java/app/EurekaServerApplication.java
+++ platform/eureka-server/src/main/java/app/EurekaServerApplication.java
@@ -1,9 +1,13 @@
 package app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
